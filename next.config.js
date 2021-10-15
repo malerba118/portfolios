854e55f0fc1cfb80b5c86a4b670bfa2a156c0eba
malerba118 <malerba118@gmail.com>
2021-10-15 16:43:13 -0600

    bleh

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ module.exports = {
     config.optimization.minimize = true;
     config.optimization.minimizer = [
       new TerserPlugin({
-        exclude: /(node_modules\/browser-image-compression)/,
+        exclude: /node_modules\/browser-image-compression/,
+        terserOptions: {
+          keep_fnames: true,
+          keep_classnames: true,
+        },
       }),
     ];
     return config;
